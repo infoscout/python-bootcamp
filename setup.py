e8c389b58836e061658ec31db7f66fb33561d321
Dana Ford <dana.ford@toponium.com>
2017-05-31 13:15:38 -0700

    Added simple setup.py module

---
+++ setup.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='python-bootcamp-pv',
+    version='0.1.0',
+    description='Package for InfoScout Python Bootcamp',
+    long_description=readme,
+    author='Dana Ford',
+    author_email='dana@infoscoutinc.com',
+    url='https://github.com/infoscout/python-bootcamp-pv',
+    license=license,
+    packages=find_packages()
+)
