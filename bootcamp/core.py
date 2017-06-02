b9965ae30eed377578ba85332df6cb397741f0a9
Dana Ford <dana.ford@toponium.com>
2017-06-01 20:18:34 -0700

    Added helper method for testing submissions

--- bootcamp/core.py
+++ bootcamp/core.py
@@ -1,2 +1,10 @@
 def say_hello():
     print "\n* Hello InfoScout Team!\n"
+
+
+def test_helper(got, expected):
+    if got == expected:
+        prefix = 'Correct!'
+    else:
+        prefix = 'Fail!'
+    print " {prefix}  got: {got} expected: {expected}".format(prefix=prefix, got=got, expected=expected)
