a747ac21026e8738fb350a03c0a4f4fca0865f93
Dana Ford <dana.ford@toponium.com>
2017-06-02 08:33:02 -0700

    Added problem for dictionaries

--- bootcamp/lesson3.py
+++ bootcamp/lesson3.py
@@ -11,8 +11,13 @@ def uniques_sorted(nums):
 
 # Question 2
 # ----------
-# Fill in: Dictionaries
-#
+# Given first name, last name, and age return a dictionary that has
+# 3 items. The keys should be 'fname', 'lname', and 'age'.
+# The values will be the function parameters. The first name and last name
+# should be title case
+def dict_profile(fname, lname, age):
+    # Write code here
+    pass
 
 
 # Question 3
@@ -25,6 +30,11 @@ def main():
     test_helper(uniques_sorted([1, 2, 3]), [1, 2, 3])
     test_helper(uniques_sorted([0, 0]), [0])
     test_helper(uniques_sorted([5, 5, 12, 13, 15, 0, -1, 12]), [-1, 0, 5, 12, 13, 15])
+
+    print "\nRunning dict_profile function..."
+    test_helper(dict_profile('walter', 'sobchak', 30), {'fname': 'Walter', 'lname': 'Sobchak', 'age': 30})
+    test_helper(dict_profile('JEFF', 'leBowski', 0), {'fname': 'Jeff', 'lname': 'Lebowski', 'age': 0})
+    test_helper(dict_profile('smokey', '', 89), {'fname': 'Smokey', 'lname': '', 'age': 89})
 
 if __name__ == '__main__':
     main()
