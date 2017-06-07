2b5fd05d9260bd1ffbe5c104ffa8d6babafe024e
Dana Ford <dana.ford@toponium.com>
2017-06-07 17:04:15 -0500

    Added some assignments to lesson 4

---
+++ bootcamp/lesson4.py
@@ -0,0 +1,31 @@
+import datetime
+import math
+
+from core import test_helper
+
+
+# Question 1
+# ----------
+# Using the datetime module return a datetime object with the year of 2015, the month of June, and the day of 1
+def playing_with_dt():
+    # Write code here
+    pass
+
+
+# Question 2
+# ----------
+# Using the math module return pi
+def playing_with_math():
+    # Write code here
+    pass
+
+
+def main():
+    print "\nRunning playing_with_dt_one function..."
+    test_helper(playing_with_dt(), datetime.datetime(2015, 06, 01))
+
+    print "\nRunning playing_with_dt_one function..."
+    test_helper(playing_with_math(), math.pi)
+
+if __name__ == '__main__':
+    main()
