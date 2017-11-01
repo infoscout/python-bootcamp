f40dd24af6788e7de7d06254850b83edb179b423
Dana Ford <dana.ford@toponium.com>
2017-11-01 09:23:34 -0700

    Revert "Added solutions for lesson 4"
    This reverts commit 58d049c78b16ec5b61f9681b605dc4e937ea7e3e.

--- bootcamp/lesson4.py
+++ bootcamp/lesson4.py
@@ -8,14 +8,16 @@ from core import test_helper
 # ----------
 # Using the datetime module return a datetime object with the year of 2015, the month of June, and the day of 1
 def playing_with_dt():
-    return datetime.datetime(2015, 06, 01)
+    # Write code here
+    pass
 
 
 # Question 2
 # ----------
 # Using the math module return pi
 def playing_with_math():
-    return math.pi
+    # Write code here
+    pass
 
 
 def main():
