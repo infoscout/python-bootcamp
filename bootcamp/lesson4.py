bc6bacf6bd5fccf2e09dd3c07f6104e1f845351b
Dana Ford <dana.ford@toponium.com>
2017-12-13 14:13:32 -0800

    Revert "Added solution to assignment 4"
    This reverts commit ddb1f3a3e37db5a216a03459e77ab7e5a7ea3d86.

--- bootcamp/lesson4.py
+++ bootcamp/lesson4.py
@@ -10,14 +10,16 @@ from core import test_helper
 # ----------
 # Using the datetime module return a datetime object with the year of 2015, the month of June, and the day of 1
 def playing_with_dt():
-    return datetime.datetime(year=2015, month=06, day=01)
+    # Write code here
+    pass
 
 
 # Question 2
 # ----------
 # Using the math module return pi
 def playing_with_math():
-    return math.pi
+    # Write code here
+    pass
 
 
 # Question 3
@@ -29,26 +31,8 @@ def playing_with_math():
 # data set as well as the percentage of ETHNICITY UNKNOWN formatted as a string with 2 significant figures.
 # The return object should be a tuple data type
 def explore_data():
-    demo_attributes = []
-    url = 'http://data.cityofnewyork.us/api/views/kku6-nxdu/rows.json?accessType=DOWNLOAD'
-
-    r = requests.get(url=url)
-    json = r.json()
-    meta = json['meta']
-    view = meta['view']
-    columns = view['columns']
-
-    for column in columns:
-        if column['name'] == 'PERCENT ETHNICITY UNKNOWN':
-            avg = column['cachedContents']['average']
-        if column['dataTypeName'] == 'number':
-            demo_attributes.append(column['name'])
-
-    num_attributes = len(set(demo_attributes))
-    avg_formatted = '{:.4f}%'.format(float(avg))
-    t = (avg_formatted, num_attributes)
-
-    return t
+    # Write code here
+    pass
 
 
 def main():
