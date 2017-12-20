3d1040a09eedad4bb831685029e140fb84c6f7dd
Dana Ford <dana.ford@toponium.com>
2017-12-20 08:43:51 -0800

    Added solutions to assignment 5

--- bootcamp/lesson5.py
+++ bootcamp/lesson5.py
@@ -6,7 +6,7 @@ import csv
 # ----------
 # Using the csv data file users.csv aggregate app users as well as registration date by month. The count of app
 # users should be one dictionary while the count of registration month should be another dictionary. There will be
-# no checking or test harness so simply print your results to the console in the display_results function.
+# no checking or test harness so simply print your results to the console.
 # Example:
 # d1 = {
 #    'RECEIPT_HOG': 3325,
@@ -20,13 +20,42 @@ import csv
 #    etc...
 # }
 def bigdata():
-    # Write code here
-    pass
+    apps_dict = {}
+    reg_dict = {}
+    file_path = '/Users/df/isc/python-bootcamp/data/users.csv'
+
+    with open(file_path, 'r') as csvreader:
+        reader = csv.reader(csvreader)
+        for row in reader:
+            user_id, verified, app_id, state, reg_str = row
+            if app_id in apps_dict:
+                apps_dict[app_id] += 1
+            else:
+                apps_dict[app_id] = 1
+
+            reg_dt = datetime.datetime.strptime(reg_str, "%Y-%m-%d %H:%M:%S")
+            reg_month = reg_dt.strftime("%b")
+
+            if reg_month in reg_dict:
+                reg_dict[reg_month] += 1
+            else:
+                reg_dict[reg_month] = 0
+
+    display_results(apps_dict, reg_dict)
 
 
 def display_results(d1, d2):
-    # Write code here
-    pass
+    h1 = "Count by Apps"
+    h2 = "Count by Registration Month"
+
+    print "\n{header}".format(header=h1)
+    print "-" * len(h1)
+    print d1
+
+    print "\n{header}".format(header=h2)
+    print "-" * len(h2)
+    print d2
+    print ""
 
 
 def main():
